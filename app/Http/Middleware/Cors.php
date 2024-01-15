20bb1c4bd81a9698f24d3c6c136248d3bcb55267
salvamorro <>
2024-01-15 17:19:26 +0100

    3.6.1

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
      {
          $response = $next($request);
  
-         $response->headers->set('Access-Control-Allow-Origin', '*');
+         $response->headers->set('Access-Control-Allow-Origin', 'https://www.alexaparicio.com');
          $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
          $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
  
